7eb694607645c7e8b3da190e3550a2996afc45ed
boobalan.ms <boobalan.ms@zettaone.com>
2023-11-28 15:23:40 +0530

    First commit "28-11-2023".

---
+++ Frontend/src/App.js
@@ -0,0 +1,18 @@
+import './App.css';
+import { Header } from './start/Header/Header';
+import { Footer } from './start/Footer/Footer';
+import { Banner } from './start/components/Banner/Banner';
+import { Sliderdiv } from './start/components/SliderDiv/Sliderdiv';
+
+function App() {
+  return (
+  <>
+  <Header />
+  <Banner />
+  <Sliderdiv />
+  <Footer />
+  </>  
+  );
+}
+
+export default App;
---
+++ Frontend/src/start/components/SliderDiv/Sliderdiv.js
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export function Sliderdiv() {
+  return (
+    <div>Sliderdiv</div>
+  )
+}
