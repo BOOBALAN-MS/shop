6cbffaa7202df8e2c5113be853f6525ec001be5c
boobalan <boobalan97ms@gmail.com>
2023-12-05 19:09:17 +0530

    tailwind added

--- Frontend/src/start/components/Banner/Banner.js
+++ Frontend/src/start/components/Banner/Banner.js
@@ -1,11 +1,12 @@
 import React from 'react';
 import styles from './Banner.module.css';
-import images from '../../../assets/images/Banner-1.jpg';
+// import images from '../../../assets/images/Banner-1.jpg';
 
 export function Banner() {
   return (
-    <div>
-      <img className={styles.images} src={images} alt="Banner" />
+    <div>      
+      {/* <img className={`mx-auto ${styles.images}`} src={images} alt="Banner" /> */}
+      <img className={`border border-amber-700 mx-auto ${styles.images}`}/>
     </div>
   )
 }
--- Frontend/src/start/components/SliderDiv/Sliderdiv.js
+++ Frontend/src/start/components/SliderDiv/Sliderdiv.js
@@ -1,7 +1,10 @@
-import React from 'react'
+import React from 'react';
+import styles from './SliderDiv.module.css';
 
 export function Sliderdiv() {
   return (
-    <div>Sliderdiv</div>
+    <div className='container mx-auto bg-red-400 border rounded px-4 p-4'>
+      <p className='text-white'>erttttttttt</p>
+    </div>
   )
 }
